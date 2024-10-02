60976e359710598e3a729dd453bdb9d67f055fbc
christalzheng <s.zheng@duke.edu>
2024-10-02 12:12:24 -0400

    ARGOS dictionary

--- data/ARGOSTrackingTool.py
+++ data/ARGOSTrackingTool.py
@@ -6,7 +6,7 @@
 #   date entered via user input.
 #
 # Author: John Fay (john.fay@duke.edu)
-# Date:   Fall 2024
+# Date:   Fall 2023
 #--------------------------------------------------------------
 
 #Create a variable pointing to the data file
@@ -22,18 +22,25 @@ line_list = file_object.readlines()
 file_object.close()
 
 #Pretend we read one line of data from the file
-lineString = line_list[199]
+date_dict={}
+location_dict = {}
+for lineString in line_list:
+    # Check if line is a data line
+    if lineString[0] in ("#","u"):
+        continue
 
-#Split the string into a list of data items
-lineData = lineString.split()
+    #Split the string into a list of data items
+    lineData = lineString.split()
 
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
 
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    date_dict[record_id] = obs_date
+    date_dict[record_id] = (obs_lat,obs_lon)
 
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
